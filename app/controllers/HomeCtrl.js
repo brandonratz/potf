3408cf085bfedb664e5f648fb4a9be25c488768f
John Tucker <john@larkintuckerllc.com>
2015-02-11 18:45:26 +0000

    added search area with call to search

--- app/controllers/HomeCtrl.js
+++ app/controllers/HomeCtrl.js
@@ -1,6 +1,9 @@
 (function() {
+
+var DIA = 2;
+
 angular.module('myApp').
-controller('HomeCtrl', ['uiGmapGoogleMapApi', '$scope', function (uiGmapGoogleMapApi, $scope) {
+controller('HomeCtrl', ['uiGmapGoogleMapApi', 'search', function (uiGmapGoogleMapApi, search) {
 	var ctrl = this;
 	ctrl.testleft = function() {
 		console.log('left');
@@ -8,13 +11,30 @@ controller('HomeCtrl', ['uiGmapGoogleMapApi', '$scope', function (uiGmapGoogleMa
 	ctrl.testright = function() {
 		console.log('right');
 	};
+	function updateLots() {
+		search.search(
+			{
+				lat: ctrl.search.center.latitude,
+				lng: ctrl.search.center.longitude,
+				start: 0,
+				end: 1
+			},
+			function(result) {
+				console.log(result);
+			},
+			function() {
+				console.log('error');
+			}
+		);
+	}; 
 	uiGmapGoogleMapApi.then(function(maps) {
 		ctrl.map = {
 			center: {
 				latitude : 29.6254423,
                         	longitude : -82.4373587
 			},
-			zoom: 13,
+	
+			// SETTING OPTIONS HERE BECAUSE OF MAPS DEFAULT CONSTANTS
 			options: {
 				disableDefaultUI: true,
 				zoomControl: true,
@@ -25,6 +45,19 @@ controller('HomeCtrl', ['uiGmapGoogleMapApi', '$scope', function (uiGmapGoogleMa
 				scaleControl: true
 			}
 		};	
-    });
+		ctrl.search = {
+			center: {
+				latitude : 29.6254423,
+                        	longitude : -82.4373587
+			},
+			radius: DIA * 1000,
+			events: {
+				dragend: function() {
+					updateLots();
+				}
+			}	
+		};
+		updateLots();
+    	});
 }]);
 })();
