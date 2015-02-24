9e9c9fe71ad0510684449942686b7ea97fea1873
John Tucker <john@larkintuckerllc.com>
2015-02-24 01:34:44 +0000

    wrapped up lotscreen

--- app/controllers/LotCtrl.js
+++ app/controllers/LotCtrl.js
@@ -2,23 +2,35 @@
 angular.module('myApp').
 controller('LotCtrl', ['$routeParams','Lots', 'navigator', 'Reservations', function ($routeParams, Lots, navigator, Reservations) {
 	var ctrl = this;
+	ctrl.reserved = false;
 	ctrl.full = false;
+	ctrl.reservations = [];
 	ctrl.navigate = navigator.navigate;
 	ctrl.start = $routeParams.start;	
 	ctrl.end = $routeParams.end;	
-	ctrl.reserve = function() {
-		var reservation = new Reservations({lot: $routeParams._id, start: ctrl.start, end: ctrl.end});
-		reservation.$save().then(function() { 
-			navigator.navigate('/');
-		})
-		.catch(function() {
-			ctrl.full = true;
-		});
-	};
 	Lots.get({_id: $routeParams._id}).then(function(data) {
 		ctrl.lot = data;
 	}).catch(function() {
 		navigator.navigate('/error');
 	});
+	function queryActiveReservations() {
+		Reservations.query({lot: $routeParams._id, active: 'true'}).then(function(data) {
+			ctrl.reservations = data;
+		}).catch(function() {
+			navigator.navigate('/error');
+		});
+	};
+	ctrl.reserve = function() {
+		var reservation = new Reservations({lot: $routeParams._id, start: ctrl.start, end: ctrl.end});
+		reservation.$save().then(function() { 
+			ctrl.reserved = true;	
+			queryActiveReservations();
+		})
+		.catch(function() {
+			ctrl.full = true;
+			queryActiveReservations();
+		});
+	};
+	queryActiveReservations();
 }]);
 })();
