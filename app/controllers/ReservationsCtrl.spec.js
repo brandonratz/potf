9d34b0a2f80ae3b83789cf3fc65c13fa1bdc184d
Adrian Ng <beaconwarden@gmail.com>
2015-06-12 18:53:08 -0400

    new files

---
+++ app/controllers/ReservationsCtrl.spec.js
@@ -0,0 +1,21 @@
+describe('ReservationsCtrl', function() {
+  beforeEach(module('myApp'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+
+  describe('$scope.grade', function() {
+    it('sets the strength to "strong" if the password length is >8 chars', function() {
+      var $scope = {};
+      var controller = $controller('ReservationsCtrl', { $scope: $scope });
+      $scope.password = 'longerthaneightchars';
+      $scope.grade();
+      expect($scope.strength).toEqual('strong');
+         expect($scope.abc).toEqual(14);
+    });
+  });
+});
